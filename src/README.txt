9e38cc38aee55ba3a5c2bc5a339768d2e6f313da
DarioRendon <dario.rendon42@gmail.com>
2016-10-25 23:46:27 -0400

    added documentatin, comments, and readded some .obj files that were accidentlly deleted

---
+++ src/README.txt
@@ -0,0 +1,6 @@
+Dario Rendon
+Xiang Fan
+
+We were able to complete the entire assignment fully
+
+The program takes in our scenegraph exactly like the starter code did in the JOGLFrame.java file
--- src/robotAnimation.java
+++ src/robotAnimation.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * Created by xiangfan on 10/25/16.
  */
 
-
+//class that contains all transformation for robot arm
 public class robotAnimation {
   private int angleOfRotation;
   private Map<String, INode> nodes;
@@ -19,26 +19,29 @@ public class robotAnimation {
     this.nodes = scenegraph.getNodes();
     this.angleOfRotation = 0;
   }
+  //add motion to a leaf
   public void animation(String name, Matrix4f matrix) {
     nodes.get(name).setAnimationTransform(matrix);
   }
-
+  //animate scene graph
   public void transform (double time) {
+    //set rotation angle
     angleOfRotation = (angleOfRotation + 1)%360;
+    //rotate the whole robot arm
     animation("robot1-whole-robot", new Matrix4f().rotate((float)Math.toRadians(angleOfRotation),0, 1,0));
     animation("robot2-whole-robot", new Matrix4f().rotate((float)Math.toRadians(angleOfRotation),0, 1,0));
-
+    //rotate lowerarm for robot1
     animation("robot1-armLower", new Matrix4f().rotate((float)Math.toRadians(Math.sin(time / 50) * 30),1, 0,0));
     animation("robot1-armLower-r", new Matrix4f().rotate((float)Math.toRadians(Math.sin(time / 50) * 30),1, 0,0));
+    //move claws for robot1
     animation("robot1-claw1-mesh", new Matrix4f().rotate((float)Math.toRadians(Math.sin(time / 20) * 40),0, 0,1));
     animation("robot1-claw2-mesh", new Matrix4f().rotate((float)Math.toRadians(- (Math.sin(time / 20) * 40)),0, 0,1));
     animation("robot1-claw1-mesh-r", new Matrix4f().rotate((float)Math.toRadians(Math.sin(time / 20) * 40),0, 0,1));
     animation("robot1-claw2-mesh-r", new Matrix4f().rotate((float)Math.toRadians(- (Math.sin(time / 20) * 40)),0, 0,1));
-
-
-
+    //rotate lowerarm for robot2
     animation("robot2-armLower", new Matrix4f().rotate((float)Math.toRadians(Math.sin(time / 50) * 30),1, 0,0));
     animation("robot2-armLower-r", new Matrix4f().rotate((float)Math.toRadians(Math.sin(time / 50) * 30),1, 0,0));
+    //move claws for robot2
     animation("robot2-claw1-mesh", new Matrix4f().rotate((float)Math.toRadians(Math.sin(time / 20) * 40),0, 0,1));
     animation("robot2-claw2-mesh", new Matrix4f().rotate((float)Math.toRadians(- (Math.sin(time / 20) * 40)),0, 0,1));
     animation("robot2-claw1-mesh-r", new Matrix4f().rotate((float)Math.toRadians(Math.sin(time / 20) * 40),0, 0,1));
